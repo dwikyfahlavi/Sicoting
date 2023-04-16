49dd4bbdb00e32442e8f96de48f537b782b84145
Dwiqy Fahlavi Muhammad Ramdani <65699938+dwikyfahlavi@users.noreply.github.com>
2023-04-16 10:23:46 +0700

    commit latihan

--- application/views/siswa/latihan.php
+++ application/views/siswa/latihan.php
@@ -6,20 +6,27 @@
         
 
         <?php 
-        $index =- 0;
+        $index = 1;
         foreach ($latihan as $m) : ?>
        <div class="section-body">
             <div class="row">
                 <div class="col-20 col-md col-lg">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Latihan <?= $index ?></h4>
+                            <h4 style="width: 99%;">Latihan <?= $index ?></h4>
+                            <?php if($hasil == null){?>
+                                <img src='<?= site_url(); ?>/assets/img/remove.png' style='width:30px;height:30px;'>
+                            <?php }else{ ?>
+                                <img src='<?= site_url(); ?>/assets/img/yes.png' style='width:30px;height:30px;'>
+                                
+                            <?php } ?>
+                            
                         </div>
                         <div class="card-body">
                             <?= $m['soal'] ?>
                         </div>
                         <div class="card-footer text-left">
-                            <a href="<?= site_url('siswa/subMateri'. '/'. $m['id_latihan'] ); ?>"><button class="btn btn-primary">Lihat Materi</button></a>
+                            <a href="<?= site_url('siswa/latihanSiswa'. '/'. $m['id_latihan'] . '/'. $idsubmateri); ?>"><button class="btn btn-primary">Kerjakan</button></a>
                             </div>
                     </div>
                 </div>
